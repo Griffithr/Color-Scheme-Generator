89f51e38a7d109949ef4d334ae554392a1a60a28
Sam-Doran <sam@samdorandigital.com>
2016-09-04 00:28:29 -0400

    Added gif that shows a lot of complementary colors and the code that makes it

---
+++ complementary.py
@@ -0,0 +1,28 @@
+from PIL import Image, ImageDraw
+
+# just rotate 180 degrees on hue
+
+
+COLOR_MODE = 'RGB'
+im_height = 200
+im_width = 200
+
+im_size = (im_height, im_width)
+
+im = Image.new(COLOR_MODE, im_size)
+
+draw = ImageDraw.Draw(im, COLOR_MODE)
+
+draw.rectangle([(0, 0), (200, 200)], fill='black')
+
+
+for i in range(360):
+    hue_val = i
+
+    draw.rectangle([(0, 0), (200, 100)], fill='hsl({}, 80%, 60%)'.format(hue_val))
+    draw.rectangle([(0, 100), (200, 200)], fill='hsl({}, 80%, 60%)'.format(hue_val + 180))
+
+    im.save('tst{}.png'.format(str(i).zfill(3)))
+
+del draw
+
