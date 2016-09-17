220df43d03b127b19070f3d7a744d134f1103f4a
Sam-Doran <sam@samdorandigital.com>
2016-09-16 20:52:54 -0400

    functions don't return original color anymore

--- generate_scheme.py
+++ generate_scheme.py
@@ -1,7 +1,6 @@
 from hsl import Hsl
 from make_img_of_scheme import img
 
-# SO to ttp://www.tigercolor.com/color-lab/color-theory/color-theory-intro.htm
 
 class Generate:
 
@@ -10,7 +9,7 @@ class Generate:
 
     def complementary(self, color):
         new_color = Hsl(h=color.h + 180, s=color.s, l=color.l)
-        return color, new_color
+        return [new_color]
 
     def monochromatic(self, color, amount_of_colors=10):
         new_colors = []
@@ -26,7 +25,6 @@ class Generate:
     def analogous(self, color, degree_dif=45):
         return [
             Hsl(h=color._h - degree_dif, s=color.s, l=color.l),
-            color,
             Hsl(h=color.h + degree_dif, s=color.s, l=color.l)
         ]
 
@@ -34,21 +32,18 @@ class Generate:
         # 120 is a third of the color wheel
         return [
             Hsl(h=color.h - 120, s=color.s, l=color.l),
-            color,
             Hsl(h=color.h + 120, s=color.s, l=color.l)
         ]
 
     def split_complementary(self, color):
         # takes complementary color and finds the two other colors next to it
         return [
-            color,
             Hsl(h=color.h + 180 - 45, s=color.s, l=color.l),
             Hsl(h=color.h + 180 + 45, s=color.s, l=color.l)
         ]
 
     def square(self, color):
         return [
-            color,
             Hsl(h=color.h + 90, s=color.s, l=color.l),
             Hsl(h=color.h + 180, s=color.s, l=color.l),
             Hsl(h=color.h - 90, s=color.s, l=color.l)
@@ -60,14 +55,21 @@ if __name__ == '__main__':
     def format_examples_path(file_name):
         return os.path.abspath('examples/') + '/' + file_name
 
+    def generate_scheme_pic(color, scheme, fl_name):
+        img([color] + scheme(color), fl_name)
+
     def generate_example_folder(fldr_name, scheme):
         for i in range(60):
-            sc_img = scheme(Hsl(h=i*6, s=100, l=50))
-            img(sc_img, format_examples_path('{}/{}.png'.format(fldr_name, str(i).zfill(3))))
+            generate_scheme_pic(
+                color=Hsl(h=i*60, s=100, l=50),
+                scheme=scheme,
+                fl_name='examples/{}/{}.png'.format(fldr_name, str(i).zfill(3))
+            )
 
+    generate_example_folder('complementary', Generate().complementary)
     generate_example_folder('triadic', Generate().triadic)
     generate_example_folder('monochromatic', Generate().monochromatic)
     generate_example_folder('analogous', Generate().analogous)
-    generate_example_folder('complementary', Generate().complementary)
     generate_example_folder('split_complementary', Generate().split_complementary)
     generate_example_folder('square', Generate().square)
+
